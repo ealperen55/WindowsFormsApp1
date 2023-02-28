0f45ad544681dcce000ace6d3cee2fd4fbb30979
Alperen <Alperen@ALPERENBUGAZ>
2023-02-28 14:05:27 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squirrel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private async void checkforupdates()
+        {
+            try
+            {
+                using (var mgr = await UpdateManager.GitHubUpdateManager("https://github.com/ealperen55/KouNotSihirbaz-"))
+                {
+                    var release = await mgr.UpdateApp();
+                }
+
+
+            }
+
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to chech updates:" + e.ToString());
+
+            }
+
+
+        }
+
+        private void AddVersionNumber()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetEntryAssembly();
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            label3.Text = $"v.{versionInfo.FileVersion}";
+
+
+        }
+    }
+}
